612822aedc60f6a9831a0b5357198ef116611371
codingwithmanny <codingwithmanny@gmail.com>
2023-06-25 18:53:17 +0300

    fix: 🐛 Fixed non-returning auth provider

--- apps/vitejs/src/providers/auth/index.tsx
+++ apps/vitejs/src/providers/auth/index.tsx
@@ -5,16 +5,18 @@ import { Auth0Provider } from "@auth0/auth0-react";
 // Root Provider
 // ========================================================
 const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-  <Auth0Provider
-    domain={import.meta.env.VITE_AUTH0_DOMAIN || window.AUTH0_DOMAIN}
-    clientId={import.meta.env.VITE_AUTH0_CLIENT_ID || window.AUTH0_CLIENT_ID}
-    authorizationParams={{
-      redirect_uri: window.location.origin,
-      audience: import.meta.env.VITE_AUTH0_AUDIENCE || window.AUTH0_AUDIENCE,
-    }}
-  >
-    {children}
-  </Auth0Provider>;
+  return (
+    <Auth0Provider
+      domain={import.meta.env.VITE_AUTH0_DOMAIN || window.AUTH0_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID || window.AUTH0_CLIENT_ID}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+        audience: import.meta.env.VITE_AUTH0_AUDIENCE || window.AUTH0_AUDIENCE,
+      }}
+    >
+      {children}
+    </Auth0Provider>
+  );
 };
 
 // Exports
